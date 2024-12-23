f7192cd011304c40c356a6314aed95f2b8435317
Trung-Logarit <trungnguyenraz@gmail.com>
2024-12-23 15:45:28 +0700

    kocogiquantrong

--- .history/src/middleware/addUserIdToQuery_20241218095637.js
+++ .history/src/middleware/addUserIdToQuery_20241218095637.js
@@ -1,4 +1,3 @@
-//- thêm user ID của người dùng đã đăng nhập vào query strin
 const catchAsync = require("../utils/catchAsync");
 exports.addUserIdToQuery = catchAsync(async (req, res, next) => {
   req.query.participants = req.user._id.toString();
