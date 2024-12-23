f0ec9bd26a41fd749632ec4a8e0e963f723f7a29
Lê Dương Khang <duongkhang2810qs@gmail.com>
2024-12-23 21:07:49 +0700

    delete history

--- .history/src/middleware/addUserIdToQuery_20241218095637.js
+++
@@ -1,5 +0,0 @@
-const catchAsync = require("../utils/catchAsync");
-exports.addUserIdToQuery = catchAsync(async (req, res, next) => {
-  req.query.participants = req.user._id.toString();
-  next();
-});
--- .history/src/middleware/addUserIdToQuery_20241223153858.js
+++
@@ -1,6 +0,0 @@
-//- thêm user ID của người dùng đã đăng nhập vào query strin
-const catchAsync = require("../utils/catchAsync");
-exports.addUserIdToQuery = catchAsync(async (req, res, next) => {
-  req.query.participants = req.user._id.toString();
-  next();
-});
